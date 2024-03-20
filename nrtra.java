a1f901bf9d55d2714b8d0a3d9b5a1e78dbf42a3d
uday <udaybhaskarrao35@gmail.com>
2024-03-20 11:51:22 +0530

    updated code

---
+++ nrtra.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+class code1
+{
+    public static void main(String args[])
+    {
+        int a = 5;
+        int c = 4;
+        int b = a + 5 + c;
+        System.out.println("the addition is "+ b);
+
+    }
+}
